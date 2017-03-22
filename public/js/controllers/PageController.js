f9554d3494210187d10ee86762db3b169937c5a9
Donny <1021001390@qq.com>
2017-03-22 10:10:37 +0800

    fix bootstrap-table table head bugs

--- public/js/controllers/PageController.js
+++ public/js/controllers/PageController.js
@@ -6,7 +6,7 @@
 
     angular.module('KPIApp.controllers')
         .controller('PageController', function (kpiService, $scope, $uibModal, $log, uuid) {
-            var host = 'http://192.168.99.240:3010/';
+            var host = 'http://192.168.1.111:3010/';
             var vm = $scope.vm = {
                 table: {
                     th: ['标题', '显示', '排序', '创建时间', '创建人', '修改时间', '修改人'],
@@ -20,8 +20,10 @@
             };
 
             var edit = function () {
-                var selects = $('#tb_pages').bootstrapTable('getSelections');
-                $scope.open(selects[0]);
+                if (vm.checked == 1) {
+                    var selects = $('#tb_pages').bootstrapTable('getSelections');
+                    $scope.open(selects[0]);
+                }
             };
 
             $scope.open = function (select) {
@@ -70,14 +72,16 @@
             };
 
             var _delete = function () {
-                var selects = $('#tb_pages').bootstrapTable('getSelections');
-                var ids = _.map(selects, 'ID');
+                if (vm.checked > 0) {
+                    var selects = $('#tb_pages').bootstrapTable('getSelections');
+                    var ids = _.map(selects, 'ID');
 
-                kpiService.post(host + 'api/kpiPages/delete', {
-                    ids: ids
-                }).then(function (res) {
-                    $('#tb_pages').bootstrapTable('refresh', {silent: true});
-                })
+                    kpiService.post(host + 'api/kpiPages/delete', {
+                        ids: ids
+                    }).then(function (res) {
+                        $('#tb_pages').bootstrapTable('refresh', {silent: true});
+                    })
+                }
             };
 
             var TableInit = function () {
@@ -91,24 +95,17 @@
                         striped: true,                      //是否显示行间隔色
                         cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
                         pagination: true,                   //是否显示分页（*）
-                        sortable: false,                    //是否启用排序
+                        sortable: true,                    //是否启用排序
                         sortOrder: "asc",                   //排序方式
                         queryParams: oTableInit.queryParams,//传递参数（*）
                         // sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
                         pageNumber: 1,                      //初始化加载第一页，默认第一页
-                        pageSize: 10,                       //每页的记录行数（*）
-                        pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+                        pageSize: 20,                       //每页的记录行数（*）
                         search: true,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
-                        strictSearch: true,
                         showColumns: true,                  //是否显示所有的列
                         showRefresh: true,                  //是否显示刷新按钮
                         minimumCountColumns: 2,             //最少允许的列数
                         clickToSelect: true,                //是否启用点击选中行
-                        height: 505,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
-                        uniqueId: "ID",                     //每一行的唯一标识，一般为主键列
-                        showToggle: true,                   //是否显示详细视图和列表视图的切换按钮
-                        cardView: false,                    //是否显示详细视图
-                        detailView: false,                  //是否显示父子表
                         columns: [{
                             checkbox: true
                         }, {
